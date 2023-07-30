2f171ece6c849fe19dd45999a16572283f19b3ed
Zack Dong <zackdong@Zacks-MacBook-Air.local>
2023-07-30 17:09:15 -0400

    Git ignore and updated key

--- web_project/settings.py
+++ web_project/settings.py
@@ -23,7 +23,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-wsqer*diceht*%i2fg)ls=c1n*-c_tfex3h_(#+q7-$d%gv@%8"
 
 SECRET_KEY = config('SECRET_KEY')
 
